aee5a66021a5a303d7381e71bdd56dfb07a88502
RedMaun <deb.il.dpkg@gmail.com>
2022-01-29 14:28:25 +0700

    пыж

--- index.js
+++ index.js
@@ -5,11 +5,30 @@ const express = require('express');
 const exphbs  = require('express-handlebars');
 const app = express();
 const path = require ('path');
-const bodyParser = require('body-parser')
-const quo = require('./models/quote')
+const bodyParser = require('body-parser');
+const { Schema, model } = require('mongoose'); 
 const urlencodedParser = bodyParser.urlencoded({ extended: true })
 const helpers = require('handlebars-helpers')();
 const dotenv = require('dotenv');
+
+function quo_iterate(glist)
+{
+  var quo_json = fs.readFileSync('../quote-bot/chats.json');
+  quo_json = JSON.parse(quo_json)
+  var quo_list = [];
+  for (var i = 0; i < quo_json.chats.length; i++)
+  {
+    temp = quo_json.chats[i][[Object.keys(quo_json.chats[i])]]
+    if (!glist.includes(temp))
+    {
+      mongoose.model(temp, new Schema({}), temp); 
+    }
+    quo_list.push(temp)
+  }
+
+  return quo_list
+}
+var quo_list = quo_iterate([])
 
 dotenv.config();
 
@@ -24,8 +43,52 @@ const options = {
 };
 
 app.get('/', async function(req, res) {
+
+  quo_list = quo_iterate(quo_list);
+
+  var chats = [];
+  for (var i = 0; i < quo_list.length; i++)
+  {
+    var quo = mongoose.model(quo_list[i]);
+    var list = await quo.find({}).lean();
+    var temp = {};
+    temp["name"] = quo_list[i];
+    temp["count"] = list.length
+    chats.push(temp);
+  }
+    res.render('main', {title: 'СЬЛРЖАЛСЧ цитатник', chats})
+
+});
+app.get('/pics/:id', (req, res) => {
+  id = req.params.id;
+  fs.readFile('./pics/' + id, function(err, data) {
+    if (err) throw err;
+    if (id.includes('.svg'))
+    {
+      res.writeHead(200, {'Content-Type': 'image/svg+xml '});
+      res.end(data); 
+    }
+    else
+    {
+      res.writeHead(200, {'Content-Type': 'image/png'});
+      res.end(data); 
+    }
+  });
+});
+
+app.get('/robots.txt', function (req, res) {
+  res.type('text/plain');
+  res.send("User-agent: *\nDisallow: /");
+});
+app.get('/all/:id', async function(req, res) {
+  var idd = String(req.params.id);
+  quo_list = quo_iterate(quo_list)
+  if (quo_list.includes(idd))
+  {
+    var quo = mongoose.model(idd)
     var list = await quo.find({}).lean();
     list = list.reverse();
+    var offset = 0
     for (var g = 0; g < list.length; g++) /*When the suffering is eternal!😳*/
     {
       if (Array.isArray(list[g].qu) && typeof list[g].qu[0] == 'object')
@@ -71,114 +134,197 @@ app.get('/', async function(req, res) {
           list[g].qu = t.flat(Infinity)
         }
       }
-    }
-    res.render('index', {title: 'СЬЛРЖАЛСЧ цитатник', list})
-})
+    }     
+    res.render('index', {title: idd, list, offset})
+  }
+  else
+  {
+    res.status(404);
 
-app.get('/index/:id', async function(req, res) {
-    var list = await quo.find({}).lean();
-    var g = Number(req.params.id);
-    if (list[g])
+  if (req.accepts('html')) {
+      res.render('404', { url: req.get('host') + req.url });
+    return;
+  }
+  }
+})
+app.get('/:id', async function(req, res) {
+    var idd = String(req.params.id);
+    quo_list = quo_iterate(quo_list)
+    if (quo_list.includes(idd))
     {
-      if (Array.isArray(list[g].qu) && typeof list[g].qu[0] == 'object')
+      var quo = mongoose.model(idd)
+      var list = await quo.find({}).lean();
+      if (list.length > 50)
       {
-        function unpack(elem, gapp)
+        var glist = list.reverse();
+        list = [];
+        for (var i = 0; i < 50; i++)
         {
-          var htm = []
-          function _unpack(e, gap)
+          list.push(glist[i])
+        }
+        var offset = glist.length - 50
+  
+      }
+      else
+      {
+        list = list.reverse();
+        var offset = 0
+      }
+      for (var g = 0; g < list.length; g++) /*When the suffering is eternal!😳*/
+      {
+        if (Array.isArray(list[g].qu) && typeof list[g].qu[0] == 'object')
+        {
+          function unpack(elem, gapp)
           {
-            return {"_id": mongoose.Types.ObjectId(), "gap": gap*15, "element":e}
-          }
-          if (Array.isArray(elem))
-          {
-            for (var i = 0; i < elem.length; i++)
+            var htm = []
+            function _unpack(e, gap)
             {
-              htm.push(unpack(elem[i], gapp+1))
+              return {"_id": mongoose.Types.ObjectId(), "gap": gap*15, "element":e}
             }
+            if (Array.isArray(elem))
+            {
+              for (var i = 0; i < elem.length; i++)
+              {
+                htm.push(unpack(elem[i], gapp+1))
+              }
+            }
+            else
+            {
+              htm.push(_unpack(elem, gapp))
+            }
+            return htm
+          }
+          function isList(List) {
+            return Array.isArray(List)
+          }
+          qu = list[g].qu
+          if (qu.every(isList))
+          {
+            var re_qu = []
+            for (var h = 0; h < qu.length; h++)
+            {
+              var a = unpack(qu[h], -1)
+              re_qu.push(a)
+            }
+            re_qu = re_qu.flat(Infinity)
+            list[g].qu = re_qu
           }
           else
           {
-            htm.push(_unpack(elem, gapp))
+            var t = unpack(qu, -1)
+            list[g].qu = t.flat(Infinity)
           }
-          return htm
         }
-        function isList(List) {
-          return Array.isArray(List)
-        }
-        qu = list[g].qu
-        if (qu.every(isList))
-        {
-          var re_qu = []
-          for (var h = 0; h < qu.length; h++)
-          {
-            var a = unpack(qu[h], -1)
-            re_qu.push(a)
-          }
-          re_qu = re_qu.flat(Infinity)
-          list[g].qu = re_qu
-        }
-        else
-        {
-          var t = unpack(qu, -1)
-          list[g].qu = t.flat(Infinity)
-        }
-      }
-      
-      res.render('result', {title:g, list: [list[g]]})
+      }     
+      res.render('index', {title: idd, list, offset})
     }
     else
     {
       res.status(404);
-      if (req.accepts('html')) {
+
+    if (req.accepts('html')) {
         res.render('404', { url: req.get('host') + req.url });
+      return;
+    }
+    }
+})
+
+app.get('/:name/:id', async function(req, res) {
+    var g = Number(req.params.id);
+    var name = String(req.params.name);
+    quo_list = quo_iterate(quo_list)
+    if (quo_list.includes(name))
+    {
+      var quo = mongoose.model(name)
+
+      var list = await quo.find({}).lean();
+      if (list[g])
+      {
+        if (Array.isArray(list[g].qu) && typeof list[g].qu[0] == 'object')
+        {
+          function unpack(elem, gapp)
+          {
+            var htm = []
+            function _unpack(e, gap)
+            {
+              return {"_id": mongoose.Types.ObjectId(), "gap": gap*15, "element":e}
+            }
+            if (Array.isArray(elem))
+            {
+              for (var i = 0; i < elem.length; i++)
+              {
+                htm.push(unpack(elem[i], gapp+1))
+              }
+            }
+            else
+            {
+              htm.push(_unpack(elem, gapp))
+            }
+            return htm
+          }
+          function isList(List) {
+            return Array.isArray(List)
+          }
+          qu = list[g].qu
+          if (qu.every(isList))
+          {
+            var re_qu = []
+            for (var h = 0; h < qu.length; h++)
+            {
+              var a = unpack(qu[h], -1)
+              re_qu.push(a)
+            }
+            re_qu = re_qu.flat(Infinity)
+            list[g].qu = re_qu
+          }
+          else
+          {
+            var t = unpack(qu, -1)
+            list[g].qu = t.flat(Infinity)
+          }
+        }
+        res.render('result', {title:g, chat:name, list: [list[g]]}) 
+      }
+      else
+      {
+        res.status(404);
+        if (req.accepts('html')) {
+            res.render('404', { url: req.get('host') + req.url });
+          return;
+        }
+      }
+    }
+    else
+    {
+      res.status(404);
+
+      if (req.accepts('html')) {
+          res.render('404', { url: req.get('host') + req.url });
         return;
       }
     }
 })
 
-app.get('/pics/:id', (req, res) => {
-    id = req.params.id;
-    fs.readFile('./pics/' + id, function(err, data) {
-        if (err) throw err;
-        if (id.includes('.svg'))
-        {
-          res.writeHead(200, {'Content-Type': 'image/svg+xml '});
-          res.end(data); 
-        }
-        else
-        {
-          res.writeHead(200, {'Content-Type': 'image/png'});
-          res.end(data); 
-        }
-  });
-});
-
-app.get('/robots.txt', function (req, res) {
-  res.type('text/plain');
-  res.send("User-agent: *\nAllow: /");
-});
-
 app.use(function(req, res, next) {
   res.status(404);
-
   if (req.accepts('html')) {
-    res.render('404', { url: req.get('host') + req.url });
+      res.render('404', { url: req.get('host') + req.url });
     return;
   }
 });
 
 async function start() {
-    try {
-      await mongoose.connect(
-        process.env.TOKEN,
-	      {
-          useNewUrlParser: true
-        }
-      )
-      https.createServer(options, app).listen(3001);
-    } catch (e) {
-      console.log(e)
-    }
+  try {
+    await mongoose.connect(
+      process.env.TOKEN,
+      {
+        useNewUrlParser: true
+      }
+    )
+    https.createServer(options, app).listen(3001);
+  } catch (e) {
+    console.log(e)
+  }
 }
   
 start();
--- public/js/main.js
+++ public/js/main.js
@@ -127,5 +127,26 @@ $( document ).ready(function() {
     }
 });
 
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 7000);
+}
+var slideIndex = 0;
+showSlides();
+
+
+
+
 // /(https?:\/\/)(([^\s\/]+)+([^\s]+)?)/ - link mathcing
 // /\[(id|club)([0-9]+)\|([^\]]+)\]/ - vkid matching
